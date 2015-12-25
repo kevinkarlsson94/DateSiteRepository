769b64120df5337e5b10d95d907915910173093a
kevinkarlsson94 <kiwii.94@hotmail.com>
2015-12-25 21:15:21 +0100

    Insert dat shit yo

---
+++ DateSite/DateSite/DateSite/Controllers/BrowseProfilesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositories;
+using DateSite.Models;
+
+namespace DateSite.Controllers
+{
+    public class BrowseProfilesController : Controller
+    {
+
+        private UsersRepository _usersRepository;
+
+        public BrowseProfilesController()
+        {
+            _usersRepository = new UsersRepository();
+        }
+
+        // GET: BrowseProfiles
+        public ActionResult Browse()
+        {
+
+            var userList = _usersRepository.fetchProfiles();
+            return View(userList);
+        }
+    }
+}
